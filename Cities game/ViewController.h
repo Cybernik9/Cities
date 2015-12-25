b00be9f782a9c83609ee24863fc46f95eccbe773
Cybernik9 <Cybernik9@gmail.com>
2015-12-25 19:06:30 +0200

    Initial Commit

---
+++ Cities game/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cities game
+//
+//  Created by Admin on 25.12.15.
+//  Copyright © 2015 HY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
